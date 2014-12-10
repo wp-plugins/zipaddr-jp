55b6b1c0e7cc13ac2670897ff55fb64451abb3c4
ta_terunuma <ta_terunuma@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-10 22:12:41 +0000

    Adding Custom Field Template Version 1.11
    git-svn-id: https://plugins.svn.wordpress.org/zipaddr-jp/trunk@1042275 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,88 @@
+=== zipaddr-jp ===
+Contributors: ta_terunuma
+Donate link: http://zipaddr2.com/wordpress/
+Tags: zipaddr, zip, address, plugin, ajax, cross-domain
+Requires at least: 2.0.2
+Tested up to: 4.0
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+The input convert an address from a zip code automatically.
+It works only in Japanese.
+
+== Description ==
+
+= In Japanese: =
+当プラグインはWordpressと住所自動入力のZipAddrをグローバル連携させるもので、日本語環境のみ動作します。<br />
+主な動作手順は次のようになります。<br />
+1.起動設定後に郵便番号の入力を待ちます。<br />
+2.Wordpress側から郵便番号データの検索リクエストをzipaddr側に送ります。<br />
+3.zipaddrでは郵便番号からDBを検索して都道府県、市区町村、地域、データをjsonp形式で返します。<br />
+4.Wordpress側にはポップアップで検索の途中データが表示されます。<br />
+5.最終的にWordpress側画面の都道府県、市区町村、地域、の各欄にデータが埋め込まれます。<br />
+詳細説明は、<br />
+[zipaddr-jp(Japanese)](http://zipaddr2.com/wordpress/)<br />
+を参照して下さい。<br />
+
+= In English: =
+Firstly zipaddr has two sites of zipaddr.com and zipaddr2.com in a service provider.
+In zipaddr.com, free site, the other are pay sites.
+
+This plugin only generates javascript sentence to cooperate with wordpress between zipaddr.
+Therefore load does one following javascript.
+http://zipaddr.com/js/zipaddr7.js or http://zipaddr2.com/js/zipaddr3.js
+SSL:(https://zipaddr-com.ssl-xserver.jp/js/zipaddr7.js or https://zipaddr2-com.ssl-xserver.jp/js/zipaddr3.js)
+
+It do the following movement.
+1.Wordpress send zip code data as a request and receive an address.
+2.zipaddr search DB from a zip code and return jsonp form for prefecture, a city, an address.
+3.A pop screen searching to wordpress screen is output.
+4.Wordpress bury each data in an address column on the Wordpress side.
+
+== Installation ==
+
+1. Upload `zipaddr-jp` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently asked questions ==
+
+
+
+== Screenshots ==
+
+1. http://zipaddr2.com/wordpress/popup.png
+
+== Changelog ==
+
+= 1.10 =
+We added the function to set an identifier for system expansion.
+
+= 1.9 =
+We added the focus function after the zip code input.
+
+= 1.8 =
+We added the font-size change function of the guidance screen.
+
+= 1.7 =
+We added the mode which managed the zip code book in your company site.
+
+= 1.0.6 =
+We added the indication point change function of the guidance screen.
+
+= 1.0.5 =
+We revised an SSL-related bug.
+
+= 1.0.4 =
+We enlarged target coverage.
+
+= 1.0.3 =
+We changed the storage place of the configuration file.
+
+= 1.0.2 =
+It added a setting function of the operation environment.
+
+= 1.0.1 =
+It register an initial version.
+
+== Upgrade notice ==
---
+++ zipaddr-jp.php
@@ -0,0 +1,94 @@
+<?php
+/*
+Plugin Name: zipaddr-jp
+Plugin URI: http://zipaddr2.com/wordpress/
+Description: The input convert an address from a zip code automatically.
+Version: 1.10
+Author: Tatsuro, Terunuma
+Author URI: http://pierre-soft.com/
+*/
+define( 'zipaddr_VERS', '1.10');
+define( 'zipaddr_PATH', dirname( __FILE__ ) );
+define( 'zipaddr_FILE1',ABSPATH."wp-content/plugins/zipaddr_define.txt" );
+
+if( is_admin() )
+	require_once zipaddr_PATH.'/admin.php';
+
+function zipaddr_jp_change($output, $opt=""){
+     if( strstr($output,'zip') == true ) {;}
+else if( strstr($output,'post')== true ) {;}
+else {return $output;}
+
+$ac = '1'; // 1:無償,2:有償,3:御社
+$kt = '5'; // 5-7:ガイダンス表示桁数
+$ta = "";  // 縦
+$yo = "";  // 横
+$pf = "";  // pc-fsize
+$sf = "";  // sp-fsize
+$fo = "";  // focus
+$si = "";  // sysid
+$fname= zipaddr_FILE1;
+if( file_exists($fname) ) { // ファイルの確認
+	$data= trim( file_get_contents($fname) );
+	$prm= explode(",", $data);
+	while( count($prm) <= 7 ) {$prm[]="";}
+	$ac= $prm[0];
+	$kt= $prm[1];
+	$ta= $prm[2];
+	$yo= $prm[3];
+	$pf= $prm[4];
+	$sf= $prm[5];
+	$fo= $prm[6];
+	$si= $prm[7];
+}
+if( $kt < "5" || "7" < $kt ) $kt= "5";
+if( $pf < 12  || 20  < $pf ) $pf= "12";
+if( $sf < 12  || 20  < $sf ) $sf= "20";
+if( isset($_SERVER['HTTPS']) ) {
+	$http= (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS']=='off' ) ?  'http' : 'https';
+}
+else $http= 'http';
+$svr= isset($_SERVER['SERVER_NAME']) ?  $_SERVER['SERVER_NAME'] : "";
+$pth= $http.'://'.$svr; // 実働環境
+$ul = 'http://zipaddr.com/js/zipaddr7.js';
+$u2 ='http://zipaddr2.com/js/zipaddr3.js';
+$uls= 'https://zipaddr-com.ssl-xserver.jp/js/zipaddr7.js';
+$u2s='https://zipaddr2-com.ssl-sixcore.jp/js/zipaddr3.js';
+$ph2='https://zipaddr2-com.ssl-sixcore.jp/css/zipaddr.css';
+$u3= $pth.'/js/zipaddr.js';
+	 if( $ac == "3" ) $lpath= $pth.'/js/zipaddr.css';
+else if( $ac == "2" ) {
+	$lpath= $pth.'/css/zipaddr.css';
+	$wk= @file_get_contents($lpath);
+	$wk2=strstr($wk,"autozip");
+	if( empty($wk) || empty($wk2) ) $lpath= $ph2; // 定義がなければ補う
+}
+else $lpath= '';
+	$ssl= '1';
+	$wp_version= get_bloginfo('version');
+	 if( $ac == "3" )  $uls= $u3;
+else if( $ac == "2" && $ssl == "1" ) $uls= $u2s;
+else if( $ac == "2" )                $uls= $u2;
+else if( $ssl == "1" ) $uls= $uls;
+else                   $uls= $ul;
+$js = '<script type="text/javascript" src="'.$uls.'?v='.zipaddr_VERS.'" charset="UTF-8"></script>';
+$js.= '<script type="text/javascript" charset="UTF-8">function zipaddr_ownb(){';
+if( !empty($ta) ) $js.= 'ZP.top='. $ta.';';
+if( !empty($yo) ) $js.= 'ZP.left='.$yo.';';
+if( !empty($pf) ) $js.= 'ZP.pfon='.$pf.';';
+if( !empty($sf) ) $js.= 'ZP.sfon='.$sf.';';
+if( !empty($fo) ) $js.= "ZP.focus='".$fo."';";
+if( !empty($si) ) $js.= "ZP.sysid='".$si."';";
+$js.= 'ZP.min='.$kt.';ZP.uver=\''.$wp_version.'\';}</script>';
+if( $ac=="2" || $ac=="3" ) $js.= '<link rel="stylesheet" href="'.$lpath.'" />';
+$ky = '<form';
+	$ans= empty($opt) ? str_ireplace($ky, $js.$ky, $output) : $output.$js;
+	return $ans;
+}
+add_filter( 'the_content', 'zipaddr_jp_change', 99999);
+
+function zipaddr_jp_usces($formtag, $type, $data){
+	return zipaddr_jp_change($formtag, "1");
+}
+add_filter( 'usces_filter_apply_addressform', 'zipaddr_jp_usces', 99999, 3);
+?>
